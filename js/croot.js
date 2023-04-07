5396c4df4cd3c7859b3d02d1871075c871b098c2
christyuda <yudacihuy09@gmail.com>
2023-04-07 13:08:15 +0700

    just change api

---
+++ js/croot.js
@@ -0,0 +1,21 @@
+import { get } from "https://jscroot.github.io/api/croot.js";
+import { setInner, addInner } from "https://jscroot.github.io/element/croot.js";
+
+function response(jsonParse) {
+    let stringtable = '';
+    jsonParse.data.forEach((user, index) => {
+      stringtable += `
+        <tr class="${index % 2 === 0 ? "bg-gray-50" : ""} text-xs">
+          <td class="pl-6 py-4">${user.id}</td>
+          <td class="pl-6 py-4">${user.email}</td>
+          <td class="pl-6 py-4">${user.first_name}</td>
+          <td class="pl-6 py-4">${user.last_name}</td>
+          <td class="pl-6 py-4"></td>
+        </tr>
+      `;
+    });
+  
+    setInner("userTable", stringtable);
+  }
+  
+  get("https://reqres.in/api/users?page=2", response);
