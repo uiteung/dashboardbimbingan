8e0912c024d51cb7ca7b5fb6695ed803f7774adc
christyuda_ <53987991+christyuda@users.noreply.github.com>
2023-04-11 13:32:59 +0700

    testing

--- js/controller/table.js
+++ js/controller/table.js
@@ -13,24 +13,24 @@ export function AmbilResponse(result) {
         
     console.log(result.data);
 
-    function tablebimbingan(result){
-        isitabelbimbingan(result);
-    }
+    // function tablebimbingan(result){
+    //     isitabelbimbingan(result);
+    // }
  
-    function isitabelbimbingan(jsonParse){
-        let row = '';
-        jsonParse.forEach((element) => {
-        row = stringtable.replace("#pembimbing1#", value.dapembimbing1)
-        .replace("#pembimbing2#", value.pembimbing2)
-        .replace("#tahun_id#", value.tahun_id)
-        .replace("#judul#", value.judul)
-        .replace("#tipe_bimbingan#", value.tipe_bimbingan)
-        .replace("#partner#", value.partner)
-        .replace("#topik#", value.topik)
-        .replace("#abstrak#", value.abstrak);
-        addChild("userTable",content);
-        addInner("userTable",row);
-        });
-    }
+    // function isitabelbimbingan(jsonParse){
+    //     let row = '';
+    //     jsonParse.forEach((element) => {
+    //     row = stringtable.replace("#pembimbing1#", value.dapembimbing1)
+    //     .replace("#pembimbing2#", value.pembimbing2)
+    //     .replace("#tahun_id#", value.tahun_id)
+    //     .replace("#judul#", value.judul)
+    //     .replace("#tipe_bimbingan#", value.tipe_bimbingan)
+    //     .replace("#partner#", value.partner)
+    //     .replace("#topik#", value.topik)
+    //     .replace("#abstrak#", value.abstrak);
+    //     addChild("userTable",content);
+    //     addInner("userTable",row);
+    //     });
+    // }
    
 }
