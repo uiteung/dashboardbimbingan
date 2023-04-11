12936035d44f91cc3a98a9f9bf105ad82dfbe8fa
christyuda_ <53987991+christyuda@users.noreply.github.com>
2023-04-11 19:48:27 +0700

    tag

--- js/template/html.js
+++ js/template/html.js
@@ -1,18 +1,6 @@
 
 export let table =`
-<tbody class="bg-white divide-y divide-gray-200">
-<tr class="text-xs font-medium text-gray-500 text-center">
-  <th class="pl-6 py-4">#pembimbing1#</th>
-  <th class="pl-6 py-4">#pembimbing2#</th>
-  <th class="pl-6 py-4">#tahun_id#</th>
-  <th class="pl-6 py-4">#judul#</th>
-  <th class="pl-6 py-4">#tipe_bimbingan#</th>
-  <th class="pl-6 py-4">#partner#</th>
-  <th class="pl-6 py-4">#link_gd#</th>
-  <th class="pl-6 py-4">#topik#</th>
-  <th class="pl-6 py-4">#abstrak#</th>
-</tr>
-</tbody>
+
 
 <tbody><tr>
                         <td class="p-0">
