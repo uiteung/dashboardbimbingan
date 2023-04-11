b4cd3b0ba278f76b61672a99554bbf157960034b
christyuda_ <53987991+christyuda@users.noreply.github.com>
2023-04-11 10:30:08 +0700

    data

--- js/controller/table.js
+++ js/controller/table.js
@@ -11,7 +11,7 @@ export const url = "https://bimit-be.ulbi.ac.id/api/v1/get_all_bimbingan";
 
 export function AmbilResponse(result) {
         
-    console.log(result);
+    console.log(result.data);
 
 
 }
