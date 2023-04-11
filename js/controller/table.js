9917c40c4320e0caef86106207d42e3757fab64b
christyuda_ <53987991+christyuda@users.noreply.github.com>
2023-04-11 15:10:11 +0700

    table

--- js/controller/table.js
+++ js/controller/table.js
@@ -1,6 +1,6 @@
 import { addInner } from "https://jscroot.github.io/element/croot.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
-import { table } from "./template/html.js"
+import { table} from "./template/html.js";
 
 export let databimbingansemester = {
     tahun_id: parseInt("20222"),
@@ -11,7 +11,10 @@ export let token = getCookie("login");
 export const url = "https://bimit-be.ulbi.ac.id/api/v1/get_all_bimbingan";
 
 export function AmbilResponse(result) {
+        
     console.log(result.data);
+   
+    function isitabelbimbingan(jsonParse){
 
     function isiTabledata(results) {
         results['data'].forEach(isitabelbimbingan);
@@ -19,18 +22,20 @@ export function AmbilResponse(result) {
     
     function isitabelbimbingan(jsonParse) {
         let row = '';
-        jsonParse.forEach((value) => {
-            row = table.replace("#pembimbing1#", value.pembimbing1)
-              .replace("#pembimbing2#", value.pembimbing2)
-              .replace("#tahun_id#", value.tahun_id)
-              .replace("#judul#", value.judul)
-              .replace("#tipe_bimbingan#", value.tipe_bimbingan)
-              .replace("#partner#", value.partner)
-              .replace("#link_gd#", value.link_gd)
-              .replace("#topik#", value.topik)
-              .replace("#abstrak#", value.abstrak);
+        jsonParse.forEach((data) => {
+            row = table.replace("#pembimbing1#", data.pembimbing1)
+              .replace("#pembimbing2#", data.pembimbing2)
+              .replace("#tahun_id#", data.tahun_id)
+              .replace("#judul#", data.judul)
+              .replace("#tipe_bimbingan#", data.tipe_bimbingan)
+              .replace("#partner#", data.partner)
+              .replace("#link_gd#", data.link_gd)
+              .replace("#topik#", data.topik)
+              .replace("#abstrak#", data.abstrak);
             addInner("userTable", row);
-        });
-    }    
-    isitabelbimbingan(result.data);
+          });
+        }    
+        isitabelbimbingan(result.data);
+
+}
 }
