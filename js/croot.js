66605fea626bfa8030560d1d80479dd68ade917d
christyuda_ <53987991+christyuda@users.noreply.github.com>
2023-04-11 10:21:37 +0700

    data bimbingan

--- js/croot.js
+++ js/croot.js
@@ -1,6 +1,6 @@
 import { get,postWithToken } from "https://jscroot.github.io/api/croot.js";
 import { setInner } from "https://jscroot.github.io/element/croot.js";
-import { token,AmbilResponse,url } from "./controller/table.js"
+import { token,AmbilResponse,url,databimbingansemester } from "./controller/table.js"
 
 
 postWithToken(url, "Login",token, databimbingansemester,AmbilResponse)
