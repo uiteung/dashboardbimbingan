105d78bcd9d3b31e80c91c1ce1deefe54253d7f9
christyuda_ <53987991+christyuda@users.noreply.github.com>
2023-04-11 14:56:17 +0700

    test

--- js/controller/table.js
+++ js/controller/table.js
@@ -1,6 +1,6 @@
-import { addInner, addChild  } from "https://jscroot.github.io/element/croot.js";
+import { addInner } from "https://jscroot.github.io/element/croot.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
-import {presensiTag,presensiClass,presensiContent} from "./template/html.js"
+import { table } from "./template/html.js"
 
 export let databimbingansemester = {
     tahun_id: parseInt("20222"),
@@ -11,30 +11,26 @@ export let token = getCookie("login");
 export const url = "https://bimit-be.ulbi.ac.id/api/v1/get_all_bimbingan";
 
 export function AmbilResponse(result) {
-        
     console.log(result.data);
 
-    function isiTabledata(results){
-        results.data.forEach(isitabelbimbingan);
+    function isiTabledata(results) {
+        results.forEach(isitabelbimbingan);
     }
-    function isitabelbimbingan(jsonParse){
+    
+    function isitabelbimbingan(jsonParse) {
         let row = '';
         jsonParse.forEach((value) => {
-            row = table.replace("#pembimbing1#", data.pembimbing1)
-              .replace("#pembimbing2#", data.pembimbing2)
-              .replace("#tahun_id#", data.tahun_id)
-              .replace("#judul#", data.judul)
-              .replace("#tipe_bimbingan#", data.tipe_bimbingan)
-              .replace("#partner#", data.partner)
-              .replace("#link_gd#", data.link_gd)
-              .replace("#topik#", data.topik)
-              .replace("#abstrak#", data.abstrak);
-              addChild ("userTable", row);
-          });
-        }    
-        isitabelbimbingan(result.data);
-
+            row = table.replace("#pembimbing1#", value.pembimbing1)
+              .replace("#pembimbing2#", value.pembimbing2)
+              .replace("#tahun_id#", value.tahun_id)
+              .replace("#judul#", value.judul)
+              .replace("#tipe_bimbingan#", value.tipe_bimbingan)
+              .replace("#partner#", value.partner)
+              .replace("#link_gd#", value.link_gd)
+              .replace("#topik#", value.topik)
+              .replace("#abstrak#", value.abstrak);
+            addInner("userTable", row);
+        });
+    }    
+    isitabelbimbingan(result.data);
 }
-
-
-
