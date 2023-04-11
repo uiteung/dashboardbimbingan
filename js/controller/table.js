8ae87f5adb4724c00edfacc7cf6505b65c766099
christyuda_ <53987991+christyuda@users.noreply.github.com>
2023-04-12 00:18:37 +0700

    change place let

--- js/controller/table.js
+++ js/controller/table.js
@@ -27,15 +27,14 @@ export function AmbilResponse(result) {
     const start = (currentPage - 1) * rowsPerPage;
     const end = start + rowsPerPage;
     const paginatedData = jsonParse.slice(start, end);
-    
-    let tahunID = data.tahun_id === "20222" ? "Tahun Ajaran Genap 2022/2023" : data.tahun_id;
-    let partnernull = data.partner === "0" ? "-" : data.partner;
-    let tipeBimbingan = data.tipe_bimbingan === "ta" ? "Tugas Akhir" : data.tipe_bimbingan;
-    let judulPendek = data.judul.substring(0, 10);
-    let topikPendek = data.topik.substring(0, 10);   
-    let abstrakPendek = data.abstrak.substring(0, 10); 
+
     paginatedData.forEach((data) => {
-		            
+		        let tahunID = data.tahun_id === "20222" ? "Tahun Ajaran Genap 2022/2023" : data.tahun_id;
+              let partnernull = data.partner === "0" ? "-" : data.partner;
+              let tipeBimbingan = data.tipe_bimbingan === "ta" ? "Tugas Akhir" : data.tipe_bimbingan;
+              let judulPendek = data.judul.substring(0, 10);
+              let topikPendek = data.topik.substring(0, 10);   
+              let abstrakPendek = data.abstrak.substring(0, 10);   
       row = table.replace("#pembimbing1#", data.pembimbing1)
         .replace("#pembimbing2#", data.pembimbing2)
         .replace("#tahun_id#", tahunID)
