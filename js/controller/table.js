f01bf3bbf66a98b8bf88a50137eaecb9424f0266
christyuda_ <53987991+christyuda@users.noreply.github.com>
2023-04-11 20:06:24 +0700

    regex

--- js/controller/table.js
+++ js/controller/table.js
@@ -18,16 +18,22 @@ export function AmbilResponse(result) {
     function isitabelbimbingan(jsonParse) {
       let row = '';
         jsonParse.forEach((data) => {
-            let judulPendek = data.judul.substring(0, 10);   
+          let tahunID = data.tahun_id === "20222" ? "Tahun Ajaran Genap 2023/2022" : data.tahun_id;
+            let partnernull = data.partner === "0" ? "-" : data.partner;
+            let tipeBimbingan = data.tahun_id === "ta" ? "Tugas Akhir" : data.tahun_id;
+            let judulPendek = data.judul.substring(0, 10);
+            let topikPendek = data.topik.substring(0, 10);   
+            let abstrakPendek = data.abstrak.substring(0, 10);   
+   
             row = table.replace("#pembimbing1#", data.pembimbing1)
               .replace("#pembimbing2#", data.pembimbing2)
               .replace("#tahun_id#", data.tahun_id)
               .replace("#judul#", judulPendek)
-              .replace("#tipe_bimbingan#", data.tipe_bimbingan)
+              .replace("#tipe_bimbingan#", tipeBimbingan)
               .replace("#partner#", data.partner)
               .replace("#link_gd#", data.link_gd)
-              .replace("#topik#", data.topik)
-              .replace("#abstrak#", data.abstrak);
+              .replace("#topik#", data.topikPendek)
+              .replace("#abstrak#", data.abstrakPendek);
             addInner("userTable", row);
           });
         }    
