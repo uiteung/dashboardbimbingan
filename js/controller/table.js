058ce997c3a9631da9b77407a4fe0d2bceac8d75
christyuda_ <53987991+christyuda@users.noreply.github.com>
2023-04-12 01:17:54 +0700

    test

--- js/controller/table.js
+++ js/controller/table.js
@@ -15,35 +15,47 @@ export function AmbilResponse(result) {
    
     function isitabelbimbingan(jsonParse) {
       let row = '';
-      let counter = 0;
       jsonParse.forEach((data) => {
         let tahunID = data.tahun_id === "20222" ? "Tahun Ajaran Genap 2022/2023" : data.tahun_id;
         let partnernull = data.partner === "0" ? "-" : data.partner;
         let tipeBimbingan = data.tipe_bimbingan === "ta" ? "Tugas Akhir" : data.tipe_bimbingan;
         let judulPendek = data.judul.substring(0, 10);
-        let topikPendek = data.topik.substring(0, 10);
-        let abstrakPendek = data.abstrak.substring(0, 10);
+        let topikPendek = data.topik.substring(0, 10);   
+        let abstrakPendek = data.abstrak.substring(0, 10);   
+    
         row += table.replace("#pembimbing1#", data.pembimbing1)
-          .replace("#pembimbing2#", data.pembimbing2)
-          .replace("#tahun_id#", tahunID)
-          .replace("#judul#", judulPendek)
-          .replace("#tipe_bimbingan#", tipeBimbingan)
-          .replace("#partner#", partnernull)
-          .replace("#link_gd#", data.link_gd)
-          .replace("#topik#", topikPendek)
-          .replace("#abstrak#", abstrakPendek);
-        counter++;
-        if (counter === 10) {
-          addInner("userTable", row);
-          row = '';
-          counter = 0;
-        }
+                    .replace("#pembimbing2#", data.pembimbing2)
+                    .replace("#tahun_id#", tahunID)
+                    .replace("#judul#", judulPendek)
+                    .replace("#tipe_bimbingan#", tipeBimbingan)
+                    .replace("#partner#", partnernull)
+                    .replace("#link_gd#", data.link_gd)
+                    .replace("#topik#", topikPendek)
+                    .replace("#abstrak#", abstrakPendek);
       });
-      if (counter > 0) {
-        addInner("userTable", row);
-      }
+    
+      setInner("userTable", row);
     }
     
+    function showPage(pageNumber, data, dataPerPage) {
+      const startIndex = (pageNumber - 1) * dataPerPage;
+      const endIndex = startIndex + dataPerPage;
+      const dataToDisplay = data.slice(startIndex, endIndex);
+      isitabelbimbingan(dataToDisplay);
+    
+      const totalPages = Math.ceil(data.length / dataPerPage);
+      const pageButtonsContainer = document.getElementById("pageButtonsContainer");
+      let buttonsHtml = '';
+    
+      for (let i = 1; i <= totalPages; i++) {
+        buttonsHtml += `<button onclick="showPage(${i}, ${JSON.stringify(data)}, ${dataPerPage})">${i}</button>`;
+      }
+    
+      setInner(pageButtonsContainer, buttonsHtml);
+    }
+    
+    // Panggil fungsi showPage untuk menampilkan data pada halaman pertama dengan 10 data per halaman
+        showPage(1, result.data, 10);   
         isitabelbimbingan(result.data);
        
   
