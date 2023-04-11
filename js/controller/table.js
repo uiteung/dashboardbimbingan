792ff1d063b1b6245ed58dab10191784e3301f85
christyuda_ <53987991+christyuda@users.noreply.github.com>
2023-04-11 10:43:23 +0700

    pembimbing1

--- js/controller/table.js
+++ js/controller/table.js
@@ -1,6 +1,6 @@
 
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
-
+import { stringtable } from "./template/html.js";
 export let databimbingansemester = {
     tahun_id: parseInt("20222"),
     tipe_bimbingan: "ta",
@@ -12,6 +12,14 @@ export const url = "https://bimit-be.ulbi.ac.id/api/v1/get_all_bimbingan";
 export function AmbilResponse(result) {
         
     console.log(result.data);
+    get(url,tablebimbingan);
+
+    function tablebimbingan(result){
+        result.forEach(isitabelbimbingan);
+    }
+    function isitabelbimbingan(value){
+        let content = stringtable.replace("#pembimbing1#", value.pembimbing1)
+    }
 
 
 }
