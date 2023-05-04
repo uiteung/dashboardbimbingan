c69433b81a0aea48f0e32710a255f68fc45f2a0d
christyuda <yudacihuy8@gmail.com>
2023-05-04 10:11:42 +0700

    redirect without cookies

--- js/controller/table.js
+++ js/controller/table.js
@@ -12,6 +12,10 @@ export const url = "https://bimit-be.ulbi.ac.id/api/v1/get_all_bimbingan";
 
 export function AmbilResponse(result) {
     console.log(result.data);
+    if (!token) {
+      window.location.href = 'iteung.ulbi.ac.id'; 
+      return; 
+  }
    
     function isitabelbimbingan(jsonParse) {
         let row = '';
